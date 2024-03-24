75faf5c01e4e3c76089e94abe55079ee5611ea04
Sebastian Henao <henao7730@gmail.com>
2024-03-23 22:17:16 -0500

    add: Card para los comentarios

---
+++ src/components/CardComments.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import "./styles/cardComments.css";
+const CardComments = ({ item }) => {
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
+
+  return (
+    <>
+        <div className='CardCommentsContainer'>
+            <div className={`CardComments ${clicked ? 'active' : ''}`} onClick={handleClick}>
+                <div className="Client">
+                    <div className="client_img">
+                        <img src={item.photo} alt="User photo" className="img-top-left"/>
+                    </div>
+                    <div className="client_text">
+                        <p>“{item.text}”</p>
+                        <div className="client_name">
+                            <h5>{item.name}</h5>
+                            <p>{item.job}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default CardComments
