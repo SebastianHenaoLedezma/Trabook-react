d2c9f478ed138dcd0b09f8b0489357aaf5852c90
Sebastian Henao <henao7730@gmail.com>
2024-03-24 20:32:18 -0500

    modificacion para el menu hambuerguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,26 +18,30 @@ const Header = () => {
           <a href='' alt="">
             <img src={logotipo} alt="Loto trabook" className='logo'/>
           </a>
-          <div className='navbar-menu'>
-            <ul className={`links ${clicked ? 'active' : ''}`}>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Destination</a></li>
-              <li><a href="#">Tour</a></li>
-              <li><a href="#">Blog</a></li>        
-            </ul>
+          <div className='menuMobile' onClick={handleClick}>
+            <div className={`burger ${clicked ? 'open' : ''}`}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
           </div>
 
-          <div className='buttons'>
-            <button className='button-login'>Login</button>
-            <button className='button-singup'>Sing Up</button>
-          </div>
+          <div className={`navbar-container ${clicked ? 'active' : ''}`}>
+            <div className='navbar-menu'>
+              <ul className='links'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Destination</a></li>
+                <li><a href="#">Tour</a></li>
+                <li><a href="#">Blog</a></li>        
+              </ul>
+            </div>
+            <div className='buttons'>
+              <button className='button-login'>Login</button>
+              <button className='button-singup'>Sing Up</button>
+            </div>
 
-          <div className="menuMobile">
           </div>
-
-          {/* Es el menu hamburguesa  */}
-          <div className={`initial ${clicked ? 'active' : ''}`}></div>
         </div>
       </nav>
     </>
